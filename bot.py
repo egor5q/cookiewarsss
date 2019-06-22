f8e059f8f077268d9455692475843851f5ce5f39
egor5q <31898567+egor5q@users.noreply.github.com>
2019-06-23 01:21:33 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -133,7 +133,7 @@ def medit(message_text,chat_id, message_id,reply_markup=None,parse_mode=None):
 
 
 def nextlvl(pet):
-    needexp=int(pet['lvl']*10+10+pet['lvl']+(pet['lvl']*10*(pet['lvl']*10)))
+    needexp=int(pet['lvl']*3+pet['lvl']+(pet['lvl']*10*(pet['lvl']*10)))
     return needexp
 
 def check1():
