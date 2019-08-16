d11afac884c762710aaf2d0570db248446414e5b
egor5q <31898567+egor5q@users.noreply.github.com>
2019-08-16 11:33:48 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -633,7 +633,7 @@ def cubeee(m):
         if 'so easy' not in chat['achievements']:
             globalchats.update_one({'id':m.chat.id},{'$push':{'achievements':'so easy'}})
             globalchats.update_one({'id':m.chat.id},{'$inc':{'pet_access':2}})
-            bot.send_message(m.chat.id, 'Открыто достижение "Так просто?"! Награда: 2 куба.)
+            bot.send_message(m.chat.id, 'Открыто достижение "Так просто?"! Награда: 2 куба.')
 
 @bot.message_handler(func=lambda message: not is_actual(message))
 def skip_message(m):
