b370b562ccee36cf5d9847995cab777f4b816037
egor5q <31898567+egor5q@users.noreply.github.com>
2020-01-29 00:37:39 +0300

    Update crocodile.py

--- crocodile.py
+++ crocodile.py
@@ -9,7 +9,6 @@ import traceback
 import requests
 from telebot import types, TeleBot
 
-import config
 
 url = 'https://translate.yandex.net/api/v1.5/tr.json/translate?'
 key = 'trnsl.1.1.20190227T075339Z.1b02a9ab6d4a47cc.f37d50831b51374ee600fd6aa0259419fd7ecd97'
