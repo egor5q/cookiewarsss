45a2d6cbc50827dcf2d06a09b86f42464e102f8f
egor5q <31898567+egor5q@users.noreply.github.com>
2020-05-01 21:58:24 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,6 +11,7 @@ from telebot import types
 token = os.environ['TELEGRAM_TOKEN']
 bot = telebot.TeleBot(token)
 
+
 client = MongoClient(os.environ['database'])
 db = client.chatpets
 users = db.users
