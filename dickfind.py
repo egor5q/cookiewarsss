acbb88d998f1f83fd1826a8808c45e51ca941616
egor5q <31898567+egor5q@users.noreply.github.com>
2020-06-29 13:24:09 +0300

    Create dickfind.py

---
+++ dickfind.py
@@ -0,0 +1,163 @@
+# -*- coding: utf-8 -*-
+import os
+import telebot
+import time
+import random
+import threading
+from emoji import emojize
+from telebot import types
+from pymongo import MongoClient
+import traceback
+
+
+token = os.environ['dickfind']
+bot = telebot.TeleBot(token)
+
+polls={}
+number=0
+
+try:
+    pass
+
+except Exception as e:
+    print('Ошибка:\n', traceback.format_exc())
+    bot.send_message(441399484, traceback.format_exc())
+
+def medit(message_text,chat_id, message_id,reply_markup=None,parse_mode=None):
+    return bot.edit_message_text(chat_id=chat_id,message_id=message_id,text=message_text,reply_markup=reply_markup,
+                                 parse_mode=parse_mode)   
+    
+@bot.message_handler(commands=['dick'])
+def dd(m):
+    global number
+    text='Угадайте, в какой коробке хуй.'
+    kb=types.InlineKeyboardMarkup(3)
+    buttons1=[]
+    buttons2=[]
+    buttons3=[]
+    amount=random.randint(1,8)
+    i=0
+    dicks=[]
+    golddicks=[]
+    while i<amount:
+        x=random.randint(1,8)
+        while x in dicks:
+            x=random.randint(1,8)
+        dicks.append(x)
+        i+=1
+    i=1
+    while i<=9:
+        randoms=random.randint(1,10000000)
+        if i in dicks:
+            if random.randint(1,100)!=1:
+                callb='penis'
+            else:
+                callb='goldpenis'
+                golddicks.append(i)
+        else:
+            callb=str(random.randint(1,100))
+        if i<=3:
+            buttons1.append(types.InlineKeyboardButton(text='📦', callback_data=callb+' '+str(number)+' '+str(randoms)))
+        elif i<=6:
+            buttons2.append(types.InlineKeyboardButton(text='📦', callback_data=callb+' '+str(number)+' '+str(randoms)))
+        elif i<=9:
+            buttons3.append(types.InlineKeyboardButton(text='📦', callback_data=callb+' '+str(number)+' '+str(randoms)))
+        i+=1
+    kb.add(*buttons1)
+    kb.add(*buttons2)
+    kb.add(*buttons3)
+    kb.add(types.InlineKeyboardButton(text='Окончить игру', callback_data='endgame '+str(number)))
+    polls.update({number:{
+        'users':{},
+        'dicks':dicks,
+        'kb':kb,
+        'golddicks':golddicks
+        
+    }}
+                )
+    bot.send_message(m.chat.id, text, reply_markup=kb)
+    number+=1
+    
+    
+
+@bot.callback_query_handler(func=lambda call:True)
+def inline(call):
+  try:
+    user=call.from_user
+    try:
+        game=polls[int(call.data.split(' ')[1])]
+    except:
+        game=None
+    if game!=None:
+        if user.id not in game['users'] and call.data!='xyi':
+            golddick=False
+            if 'penis' in call.data:
+                dick=True
+                if 'gold' in call.data:
+                    golddick=True
+                    text='🍌|Ура! Вы нашли золотой пенис!'
+                else:
+                    text='🍆|Ура! Вы выбрали ящик с членом!'
+                bot.answer_callback_query(call.id, text, show_alert=True)
+            else:
+                dick=False
+                bot.answer_callback_query(call.id, '💨|О нет! Вы выбрали ящик без члена!', show_alert=True)
+            
+            game['users'].update({user.id:{'name':call.from_user.first_name,
+                                          'dick':dick,
+                                          'golddick':golddick}})
+            kb=types.InlineKeyboardMarkup(3)
+            
+            medit(editmsg(game), call.message.chat.id, call.message.message_id, reply_markup=game['kb'])
+        
+        elif 'endgame' not in call.data:
+            bot.answer_callback_query(call.id, 'Вы уже походили!')
+        
+    if 'endgame' in call.data:
+        kb2=types.InlineKeyboardMarkup()
+        buttons1=[]
+        buttons2=[]
+        buttons3=[]
+        i=1
+        while i<=9:
+            if i in game['dicks']:
+                emoj='🍆'
+                if i in game['golddicks']:
+                    emoj='🍌'
+            else:
+                emoj='💨'
+            if i<=3:
+                buttons1.append(types.InlineKeyboardButton(text=emoj, callback_data='xyi'))
+            elif i<=6:
+                buttons2.append(types.InlineKeyboardButton(text=emoj, callback_data='xyi'))
+            elif i<=9:
+                buttons3.append(types.InlineKeyboardButton(text=emoj, callback_data='xyi'))
+            i+=1
+        kb2.add(*buttons1)
+        kb2.add(*buttons2)
+        kb2.add(*buttons3)
+        result=editmsg(game, True)
+        medit('Игра окончена юзером '+call.from_user.first_name+'! Результаты:\n'+result, call.message.chat.id, call.message.message_id, reply_markup=kb2)
+
+  except Exception as e:
+    bot.send_message(441399484, traceback.format_exc())
+    
+def editmsg(game, end=False):
+    if end==False:
+        text='Угадайте, в какой коробке хуй.\n\n'
+    else:
+        text=''
+    for ids in game['users']:
+        if game['users'][ids]['golddick']==True:
+            text+=game['users'][ids]['name']+': 🍌нашёл(ла) ЗОЛОТОЙ член!\n'
+        
+        elif game['users'][ids]['dick']==True:
+            text+=game['users'][ids]['name']+': 🍆нашёл(ла) член\n'
+        else:
+            text+=game['users'][ids]['name']+': 💨открыл(а) пустую коробку\n'
+    return text
+    
+    
+    
+print('7777')
+bot.polling(none_stop=True,timeout=600)
